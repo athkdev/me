7ef2beb71d4338ddcc8a60a67e082ac15f8acb78
AtharvaKamble <atharvadkamble00@gmail.com>
2023-03-29 15:53:59 +0530

    feat: setup redirecto from /leetcode to my notion.site

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: "./build",
+  async redirects() {
+    return [
+      {
+        source: "/leetcode",
+        destination:
+          "https://adkio.notion.site/adkio/Leetcode-w-me-753a277aeab2456d9773478ef0d11a3c",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
