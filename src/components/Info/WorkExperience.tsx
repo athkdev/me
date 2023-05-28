8b6b265c1e52d08c65c4b7abe4f58c2329fc27cc
AtharvaKamble <atharvadkamble00@gmail.com>
2023-05-28 19:53:43 +0530

    chore: add company name and link

--- src/components/Info/WorkExperience.tsx
+++ src/components/Info/WorkExperience.tsx
@@ -4,6 +4,9 @@ import Details from "./Details";
 import BulletedList from "./BulletedList";
 import Title from "./Title";
 
+import { Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+
 interface WorkExperienceProps {
   className: string;
 }
@@ -15,6 +18,8 @@ interface Job {
   current: boolean;
   description: string;
   techStack: string[];
+  company: string;
+  companyLink: string;
 }
 
 const jobs: Job[] = [
@@ -24,7 +29,9 @@ const jobs: Job[] = [
     end: "",
     current: true,
     description: "Building web e2e products!",
-    techStack: ["Spring Boot", "AWS", "Angular"],
+    techStack: ["Spring Boot", "AWS", "Angular", "Docker"],
+    company: "Cognizant Technology Solutions",
+    companyLink: "https://www.cognizant.com/in/en",
   },
   {
     title: "Software Engineer Intern",
@@ -34,6 +41,8 @@ const jobs: Job[] = [
     description:
       "Built e-commerce products for Shopify, loyalty rewards apps for local businesses in Dublin, Ireland.",
     techStack: ["Nextjs", "React", "Express", "GraphQL", "TS"],
+    company: "RaftLabs Co.",
+    companyLink: "https://www.raftlabs.co",
   },
 ];
 
@@ -42,29 +51,45 @@ export default function WorkExperience({ className }: WorkExperienceProps) {
     <div className={`${className}`}>
       <p className="mb-2">Work experience</p>
       <div className="grid grid-row-2 sm:grid-cols-6 my-4">
-        {jobs.map(({ start, end, title, current, description, techStack }) => {
-          return (
-            <>
-              <DateRange
-                start={start}
-                current={current}
-                end={end}
-                className="mb-1"
-              />
-              <Details>
-                <Title name={title} className="mb-1" />
-                <Description>
-                  <p className="text-neutral-400">{description}</p>
-                  <BulletedList
-                    className="mt-2"
-                    key={title.toLowerCase()}
-                    array={techStack}
-                  />
-                </Description>
-              </Details>
-            </>
-          );
-        })}
+        {jobs.map(
+          ({
+            start,
+            end,
+            title,
+            current,
+            description,
+            techStack,
+            company,
+            companyLink,
+          }) => {
+            return (
+              <>
+                <DateRange
+                  start={start}
+                  current={current}
+                  end={end}
+                  className="mb-1"
+                />
+                <Details>
+                  <span>
+                    <Title name={title} />
+                    <Link className="text-xs" href={companyLink} isExternal>
+                      {company} <ExternalLinkIcon mx="2px" />
+                    </Link>
+                  </span>
+                  <Description>
+                    <p className="text-neutral-400 mt-2">{description}</p>
+                    <BulletedList
+                      className="mt-2"
+                      key={title.toLowerCase()}
+                      array={techStack}
+                    />
+                  </Description>
+                </Details>
+              </>
+            );
+          }
+        )}
       </div>
     </div>
   );
