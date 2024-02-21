458080d6bcf945c4793a9822aef8b347e67590f4
AtharvaKamble <atharvadkamble00@gmail.com>
2024-02-21 14:31:09 -0500

    chore: updated projects

--- src/components/Info/ProjectCard.tsx
+++ src/components/Info/ProjectCard.tsx
@@ -5,6 +5,7 @@ interface ProjectCardProps {
   key: string;
   title: string;
   url: string;
+  desc: string;
   className?: string;
 }
 
@@ -12,15 +13,18 @@ export default function ProjectCard({
   key,
   title,
   url,
+  desc, 
   className,
 }: ProjectCardProps): ReactElement {
   return (
     <Link href={url} isExternal>
       <div
         key={key}
-        className={`p-2 relative rounded border-2 border-white text-white font-semibold hover:-translate-y-1 hover:bg-amber-500 transition hover:ease-in-out ${className}`}
+        className={`p-2 relative flex flex-col justify-between rounded border-2 border-white text-white font-semibold h-48 hover:-translate-y-1 hover:bg-amber-500 transition hover:ease-in-out ${className}`}
+        title={desc}
       >
-        {title}
+        <span className="text-base font-extrabold">{title}</span>
+        <span className="text-sm font-thin no-underline">{desc}</span>
         <p className="bg-white text-black mt-2 text-xs"></p>
       </div>
     </Link>
--- src/components/Info/Projects.tsx
+++ src/components/Info/Projects.tsx
@@ -2,44 +2,50 @@ import { ReactElement } from "react";
 import ProjectCard from "./ProjectCard";
 
 interface ProjectProps {
-  className?: string;
+    className?: string;
 }
 
 interface Project {
-  title: string;
-  url: string;
+    title: string;
+    desc: string;
+    url: string;
 }
 
 const projects: Project[] = [
-  {
-    title: "This site!",
-    url: "https://github.com/AtharvaKamble/atharvakamble.github.io",
-  },
-  {
-    title: "cmto",
-    url: "https://github.com/AtharvaKamble/cmto",
-  },
-  {
-    title: "Relations builder",
-    url: "https://github.com/AtharvaKamble/relation-builder",
-  },
-  {
-    title: "TLL - The Learners' Language",
-    url: "https://github.com/AtharvaKamble/tll",
-  },
+    {
+        title: "This site!",
+        desc: "Built in Next.js, hosted on GitHub pages, this site is kind of like the initial screening of knowing me - to know me more, lets network!",
+        url: "https://github.com/AtharvaKamble/atharvakamble.github.io",
+    },
+    {
+        title: "Present",
+        desc: "A presentation app designed beautifully by keeping an animation-first approach in mind making it easier to make academic presentations",
+        url: "https://present-self.vercel.app/landing",
+    },
+    {
+        title: "Relations builder",
+        desc: "Allows users to add people and add relations between them somewhat close to social networks like FaceBook, LinkedIn, Instagram",
+        url: "https://github.com/AtharvaKamble/relation-builder",
+    },
+    {
+        title: "TLL - The Learners' Language",
+        desc: "A language compiler written in Java as a passion project that implements easier syntax by trying to get as close to everyday lingo",
+        url: "https://github.com/AtharvaKamble/tll",
+    },
+
 ];
 
 export default function Projects({ className }: ProjectProps): ReactElement {
-  return (
-    <div className={`${className}`}>
-      <p className="mb-2">Projects</p>
-      <div className="grid grid-row-2 sm:grid-cols-4 gap-4 my-4">
-        {projects?.map(({ title, url }) => {
-          return (
-            <ProjectCard key={title.toLowerCase()} title={title} url={url} />
-          );
-        })}
-      </div>
-    </div>
-  );
+    return (
+        <div className={`${className}`}>
+            <p className="mb-2">Projects</p>
+            <div className="grid grid-row-2 sm:grid-cols-4 gap-4 my-4">
+                {projects?.map(({ title, url, desc }) => {
+                    return (
+                        <ProjectCard key={title.toLowerCase()} title={title} url={url} desc={desc} />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
