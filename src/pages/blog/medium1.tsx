602d854dc1b529f78d08bfef3c82c22ba7da21ec
AtharvaKamble <atharvadkamble00@gmail.com>
2024-05-14 15:38:44 -0400

    (content): add medium1.tsx blog post page

---
+++ src/pages/blog/medium1.tsx
@@ -0,0 +1,11 @@
+import Renderer from "@/components/Blog/Renderer";
+
+export default function Medium1() {
+  return (
+    <section className="min-h-screen">
+      <div className="mx-auto max-w-3xl">
+        <Renderer />
+      </div>
+    </section>
+  );
+}
