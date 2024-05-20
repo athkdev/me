c490a643628d0649a2905881b710ea1cefd55a44
AtharvaKamble <atharvadkamble00@gmail.com>
2024-05-19 21:00:44 -0400

    (deployment): add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Contact from "@/components/Contact";
 import Nav from "@/components/nav";
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 import { ChakraProvider, Divider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Divider />
         <Contact open={resumeModalOpen} setOpen={setResumeModalOpen} />
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ChakraProvider>
   );
