a521d87270e79f4e5cabd0ca87d1f824477596db
AtharvaKamble <atharvadkamble00@gmail.com>
2024-05-26 02:43:26 -0400

    typo

--- src/components/Info/About.tsx
+++ src/components/Info/About.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <div>
       <p className="mb-1">About me</p>
       <p className="text-neutral-400">
-        I&apos;m Atharva. I am a software developer based out of Boston.I
+        I&apos;m Atharva. I am a software developer based out of Boston. I
         specialize in fullstack engineering, application development and cloud
         infrastructure. I am an AI enthusiast and absolutely love prompt
         engineering!
