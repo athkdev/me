bc8e8c0eed8b6bb7db25dd5f469a34dca7283201
AtharvaKamble <atharvadkamble00@gmail.com>
2024-10-03 14:16:00 -0400

    add: resume and change resume from cdn to local

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div className="min-h-screen">
       <iframe
-        src="https://cdn.athk.dev/pdf/resume.pdf"
+        src="/cdn/resume.pdf"
         className="min-h-screen w-full"
       />
       <GoogleAnalytics gaId="G-DS9411FNK4" />
