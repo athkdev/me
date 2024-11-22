8b80f63dbb5ca2aa2666ec9de2b00a3bdfd067fa
AtharvaKamble <atharvadkamble00@gmail.com>
2024-11-22 16:27:16 -0500

    about updated

--- src/components/Info/About.tsx
+++ src/components/Info/About.tsx
@@ -3,15 +3,18 @@ export default function About() {
     <div>
       <p className="mb-1">About me</p>
       <p className="text-neutral-400">
-        I&apos;m Atharva. I am a software developer based out of Boston. I
+        I&apos;m Atharva. I am a software engineer based out of Boston, MA. I
         specialize in fullstack engineering, application development and cloud
-        infrastructure. I am an AI enthusiast and absolutely love prompt
+        infrastructure. I am an AI enthusiast with a focus on Gen AI and transformer architecture with PyTorch and absolutely love prompt
         engineering!
       </p>
       <p className="text-neutral-400 mt-2">
         I enjoy building dynamic, creative products from start to finish.
         Focused on developing intuitive experiences that constantly grow and
         improve based on user metrics. Always shipping!
+      </p>
+      <p className="text-neutral-400 mt-2">
+        I am a HUGE mechanical keyboard enthusiast. At the moment, I am using the <a className="italic underline underline-offset-4" href="https://vortexgear.store/products/tab-60?srsltid=AfmBOooYgD4XA-f6FmRM8ZQ-Z4G1SRncRbrmUYf_oyIoFuyIx4XjGmMV">Vortexgear Tab 60</a> with Gateron Brown keys.
       </p>
     </div>
   );
